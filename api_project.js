cfa1f6706615de5221f51d13f8a511671f0c4652
JuHyuk Park <creduo@gmail.com>
2021-07-31 23:45:41 +0900

    apidocjs - shopping
    요청타입 구현이 되지 않으므로 생성된 문서에서는 sample request 를 시용할 수 없습니다. (json sample을 복사, 수정해서는 가능)

--- api_project.js
+++ api_project.js
@@ -3,11 +3,11 @@ define({
   "version": "0.8.0",
   "description": "쉐어밸류에서 제공하는 스마트 부킹 플러스 API 문서입니다.",
   "title": "SmartBookingPlus API",
-  "url": "https://api.smartbookingplus.com",
-  "sampleUrl": "https://apidoc.smartbookingplus.com",
+  "url": "https://docs.smartbookingplus.com",
+  "sampleUrl": "https://api.sharevalue.co",
   "header": {
     "title": "시작하기",
-    "content": "<h1></h1>\n<p>There</p>\n"
+    "content": "<h1>Hi</h1>\n<p>There</p>\n"
   },
   "footer": {
     "title": "용어집",
@@ -22,7 +22,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-07-28T06:28:47.528Z",
+    "time": "2021-07-31T14:43:17.605Z",
     "url": "https://apidocjs.com",
     "version": "0.28.1"
   }
