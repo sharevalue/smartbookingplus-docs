0e126a65dbb0a8bcfe6a576d465e018f9eb85fb8
daeheekim <dhkim@sharevaluecorp.com>
2023-05-09 11:17:33 +0900

    v1 API 수정

--- api_project.js
+++ api_project.js
@@ -3,16 +3,8 @@ define({
   "version": "1.0.1",
   "description": "쉐어밸류에서 제공하는 스마트 부킹 플러스 API 문서입니다.",
   "title": "SmartBookingPlus API",
-  "url": "https://api-manager.smartbookingplus.com",
+  "url": "https://api.smartbookingplus.com",
   "sampleUrl": "https://api-dev.smartbookingplus.com",
-  "header": {
-    "title": "시작하기",
-    "content": ""
-  },
-  "footer": {
-    "title": "용어집",
-    "content": ""
-  },
   "template": {
     "withCompare": true,
     "withGenerator": true,
@@ -22,7 +14,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2023-04-26T06:29:56.122Z",
+    "time": "2023-05-09T02:16:57.628Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
